1f1b0bf25f3c837ac29a85775e684480600ef598
mpalysiewicz <palysiewicz.miroslaw@gmail.com>
2012-03-12 11:35:33 +0100

    SeleniumGrid.bat modified and xml suites cleanup

--- SeleniumGrid.bat
+++ SeleniumGrid.bat
@@ -1,14 +1,13 @@
-set seleniumGrid=C:\Users\Mirek\workspace\WebDriver\lib\selenium-2.19.0
-
+set seleniumGrid=C:\Users\Mirek\workspace\WebDriver\lib\selenium-2.18.0
+set seleniumVersion=2.18.0
 ECHO Load GitHub
 
-
 cd %seleniumGrid%
-START java -jar selenium-server-standalone-2.18.0.jar -role hub
-START java -jar selenium-server-standalone-2.18.0.jar -role webdriver -hub http://localhost:4444/grid/register -browser browserName=iexplore,platform=WINDOWS -port 4448
-START java -jar selenium-server-standalone-2.18.0.jar -role webdriver -hub http://localhost:4444/grid/register -browser browserName=firefox,platform=WINDOWS
-START java -jar selenium-server-standalone-2.18.0.jar -role webdriver -hub http://localhost:4444/grid/register -browser browserName=firefox,platform=WINDOWS -port 4445
-START java -jar selenium-server-standalone-2.18.0.jar -role webdriver -hub http://localhost:4444/grid/register -browser browserName=chrome,platform=WINDOWS
+START java -jar selenium-server-standalone-%seleniumVersion%.jar -role hub
+START java -jar selenium-server-standalone-%seleniumVersion%.jar -role webdriver -hub http://localhost:4444/grid/register -browser browserName=iexplore,platform=WINDOWS -port 4448
+START java -jar selenium-server-standalone-%seleniumVersion%.jar -role webdriver -hub http://localhost:4444/grid/register -browser browserName=firefox,platform=WINDOWS
+START java -jar selenium-server-standalone-%seleniumVersion%.jar -role webdriver -hub http://localhost:4444/grid/register -browser browserName=firefox,platform=WINDOWS -port 4445
+START java -jar selenium-server-standalone-%seleniumVersion%.jar -role webdriver -hub http://localhost:4444/grid/register -browser browserName=chrome,platform=WINDOWS
 
 ECHO Type something
 
